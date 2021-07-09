6e2b292bc7e6279983479625f6c5d9a38b6961c4
marceaguerrero <31277734+marceaguerrero@users.noreply.github.com>
2021-07-09 02:50:58 -0300

    Add files via upload

--- TP1/modulo-curvas.js
+++ TP1/modulo-curvas.js
@@ -1,3 +1,4 @@
+
     // Definimos las Bases de Berstein, dependen de u
     function Base0(u) { return 0.5*(1-u)*(1-u);}   // 0.5*(1-u)^2
     function Base1(u) { return 0.5+u*(1-u);} 		// 0.5+ u*(1-u)
@@ -16,7 +17,7 @@
             "y":0.,
             "z":0.
           }				
-		punto.x=Base0(u)*p0[0]+Base1(u)*p1[0]+Base2(u)*p2[0];
+        punto.x=Base0(u)*p0[0]+Base1(u)*p1[0]+Base2(u)*p2[0];
 		punto.y=Base0(u)*p0[1]+Base1(u)*p1[1]+Base2(u)*p2[1];
 		punto.z=Base0(u)*p0[2]+Base1(u)*p1[2]+Base2(u)*p2[2];
 		return punto;
@@ -39,7 +40,10 @@
 
     function devuelvoCurva(puntosDeControl, tamanio){
         var resultado=[];
-        u = [0., 0.25, 0.5, 0.75, 1.];
+        //u = [0., 0.25, 0.5, 0.75, 1.];
+        var deltaU=0.01; // es el paso de avance sobre la curva cuanto mas chico mayor es el detalle
+        // u=0.05 son 20 segmentos (0.05=1/20)
+        var primer_punto;
         //tamanio = puntosDeControl.lenght();
         for(n=0;n<tamanio/3;n++)
             {
@@ -47,8 +51,18 @@
             tramo.push(puntosDeControl[n*3]);
             tramo.push(puntosDeControl[n*3 + 1]);
             tramo.push(puntosDeControl[n*3 + 2]);
-            for(i=0.;i<5;i++)
-                {resultado.push(curvaCuadratica (u[i],puntosDeControl));}
+            if (puntosDeControl[n*3 + 2])
+                for (var u=0;u<1.001;u=u+deltaU){
+                    resultado.push(curvaCuadratica(u,tramo));
+
+                    if (!primer_punto)
+                    //guardo el primero
+                        { primero = curvaCuadratica(u,tramo);
+                          primer_punto = 1.;}
+                    }
             }
+        //el ultimo tiene que ser igual al primero
+        resultado.push(primero);
+        //console.log(resultado);
         return resultado;
     }
--- TP1/perlin-noise.js
+++ TP1/perlin-noise.js
@@ -1,184 +1,120 @@
+  var vec3=glMatrix.vec3;
+  var vec4=glMatrix.vec4;
+
+  function getPerlin(rect_invi, cantidad){
+    var resultado = []
+    var delta;        
+    var primer_punto;
+
+    //console.log(rect_invi);
+    for (i=0; i< cantidad; i++)
+    {
+          delta = applyPerlin(rect_invi[i][0], rect_invi[i][1],rect_invi[i][2]);
+          //delta = Math.abs(delta)  ;
+          resultado.push([rect_invi[i][0]+delta, rect_invi[i][1],rect_invi[i][2]+delta]);
+          if (!primer_punto)
+          //guardo el primero
+              { primero = ([rect_invi[i][0]+delta, rect_invi[i][1],rect_invi[i][2]+delta]);
+                primer_punto = 1.;}
+    }
+        //el ultimo tiene que ser igual al primero
+    resultado.push(primero);
+    resultado.push(primero);
+    //console.log(resultado);
+    return resultado;
+
+//    function fixedrandom(p){
+ //     const seed = 43758.5453123;
+  //    return p+(Math.abs(Math.sin(p)) * seed)%1;
+   // }
+
+  }
+
+  function applyPerlin(x,y,z){
+
+           var PERLIN_YWRAPB = 4;
+           var PERLIN_YWRAP = 1 << PERLIN_YWRAPB;
+           var PERLIN_ZWRAPB = 8;
+           var PERLIN_ZWRAP = 1 << PERLIN_ZWRAPB;
+           var PERLIN_SIZE = 4095;
+           var perlin_octaves = 4; // default to medium smooth
+           var perlin_amp_falloff = 0.5; // 50% reduction/octave
+           var scaled_cosine = function scaled_cosine(i) {
+             return 0.5 * (1.0 - Math.cos(i * Math.PI));
+           };
+           var perlin; // will be initialized lazily by noise() or noiseSeed()
+             if (perlin == null) {
+               perlin = new Array(PERLIN_SIZE + 1);
+               for (var i = 0; i < PERLIN_SIZE + 1; i++) {
+                 perlin[i] = Math.random();
+               }
+             }
  
-// Alea random number generator.
-//----------------------------------------------------------------------------//
-
-    // From http://baagoe.com/en/RandomMusings/javascript/
-    function Alea() {
-      return (function(args) {
-        // Johannes BaagÃ¸e <baagoe@baagoe.com>, 2010
-        var s0 = 0;
-        var s1 = 0;
-        var s2 = 0;
-        var c = 1;
-
-        if (args.length == 0) {
-          args = [+new Date];
-        }
-        var mash = Mash();
-        s0 = mash(' ');
-        s1 = mash(' ');
-        s2 = mash(' ');
-
-        for (var i = 0; i < args.length; i++) {
-          s0 -= mash(args[i]);
-          if (s0 < 0) {
-            s0 += 1;
-          }
-          s1 -= mash(args[i]);
-          if (s1 < 0) {
-            s1 += 1;
-          }
-          s2 -= mash(args[i]);
-          if (s2 < 0) {
-            s2 += 1;
-          }
-        }
-        mash = null;
-
-        function random() {
-          var t = 2091639 * s0 + c * 2.3283064365386963e-10; // 2^-32
-          s0 = s1;
-          s1 = s2;
-          return s2 = t - (c = t | 0);
-        };
-        
-        random.uint32 = function() {
-          return random() * 0x100000000; // 2^32
-        };
-        random.fract53 = function() {
-          return random() + 
-            (random() * 0x200000 | 0) * 1.1102230246251565e-16; // 2^-53
-        };
-        random.version = 'Alea 0.9';
-        random.args = args;
-        return random;
-
-      } (Array.prototype.slice.call(arguments)));
-    };
-
-    // From http://baagoe.com/en/RandomMusings/javascript/
-    // Johannes BaagÃ¸e <baagoe@baagoe.com>, 2010
-    function Mash() {
-      var n = 0xefc8249d;
-
-      var mash = function(data) {
-        data = data.toString();
-        for (var i = 0; i < data.length; i++) {
-          n += data.charCodeAt(i);
-          var h = 0.02519603282416938 * n;
-          n = h >>> 0;
-          h -= n;
-          h *= n;
-          n = h >>> 0;
-          h -= n;
-          n += h * 0x100000000; // 2^32
-        }
-        return (n >>> 0) * 2.3283064365386963e-10; // 2^-32
-      };
-
-      mash.version = 'Mash 0.9';
-      return mash;
-    }
-
-// Classic Perlin noise, 3D version 
-//----------------------------------------------------------------------------//
-
-    function getPerlin(rect_invi, cantidad){
-        var resultado = []
-        for (i=0; i< cantidad; i++)
-        {
-            resultado.push(ClassicalNoise(rect_invi[i]));
-        }
-        return resultado;
-    }
-
-    function ClassicalNoise(r) { // Classic Perlin noise in 3D, for comparison 
-	    if (r == undefined) r = Math;
-      this.grad3 = [[1,1,0],[-1,1,0],[1,-1,0],[-1,-1,0], 
-                                     [1,0,1],[-1,0,1],[1,0,-1],[-1,0,-1], 
-                                     [0,1,1],[0,-1,1],[0,1,-1],[0,-1,-1]]; 
-      this.p = [];
-      for (var i=0; i<256; i++) {
-	      this.p[i] = Math.floor(Math.random()*256);
-      }
-      // To remove the need for index wrapping, double the permutation table length 
-      this.perm = []; 
-      for(var i=0; i<512; i++) {
-		    this.perm[i]=this.p[i & 255];
-      }
-    };
-
-    ClassicalNoise.prototype.dot = function(g, x, y, z) { 
-        return g[0]*x + g[1]*y + g[2]*z; 
-    };
-
-    ClassicalNoise.prototype.mix = function(a, b, t) { 
-        return (1.0-t)*a + t*b; 
-    };
-
-    ClassicalNoise.prototype.fade = function(t) { 
-        return t*t*t*(t*(t*6.0-15.0)+10.0); 
-    };
-
-    ClassicalNoise.prototype.noise = function(x, y, z) { 
-      // Find unit grid cell containing point 
-      var X = Math.floor(x); 
-      var Y = Math.floor(y); 
-      var Z = Math.floor(z); 
-      
-      // Get relative xyz coordinates of point within that cell 
-      x = x - X; 
-      y = y - Y; 
-      z = z - Z; 
-      
-      // Wrap the integer cells at 255 (smaller integer period can be introduced here) 
-      X = X & 255; 
-      Y = Y & 255; 
-      Z = Z & 255;
-      
-      // Calculate a set of eight hashed gradient indices 
-      var gi000 = this.perm[X+this.perm[Y+this.perm[Z]]] % 12; 
-      var gi001 = this.perm[X+this.perm[Y+this.perm[Z+1]]] % 12; 
-      var gi010 = this.perm[X+this.perm[Y+1+this.perm[Z]]] % 12; 
-      var gi011 = this.perm[X+this.perm[Y+1+this.perm[Z+1]]] % 12; 
-      var gi100 = this.perm[X+1+this.perm[Y+this.perm[Z]]] % 12; 
-      var gi101 = this.perm[X+1+this.perm[Y+this.perm[Z+1]]] % 12; 
-      var gi110 = this.perm[X+1+this.perm[Y+1+this.perm[Z]]] % 12; 
-      var gi111 = this.perm[X+1+this.perm[Y+1+this.perm[Z+1]]] % 12; 
-      
-      // The gradients of each corner are now: 
-      // g000 = grad3[gi000]; 
-      // g001 = grad3[gi001]; 
-      // g010 = grad3[gi010]; 
-      // g011 = grad3[gi011]; 
-      // g100 = grad3[gi100]; 
-      // g101 = grad3[gi101]; 
-      // g110 = grad3[gi110]; 
-      // g111 = grad3[gi111]; 
-      // Calculate noise contributions from each of the eight corners 
-      var n000= this.dot(this.grad3[gi000], x, y, z); 
-      var n100= this.dot(this.grad3[gi100], x-1, y, z); 
-      var n010= this.dot(this.grad3[gi010], x, y-1, z); 
-      var n110= this.dot(this.grad3[gi110], x-1, y-1, z); 
-      var n001= this.dot(this.grad3[gi001], x, y, z-1); 
-      var n101= this.dot(this.grad3[gi101], x-1, y, z-1); 
-      var n011= this.dot(this.grad3[gi011], x, y-1, z-1); 
-      var n111= this.dot(this.grad3[gi111], x-1, y-1, z-1); 
-      // Compute the fade curve value for each of x, y, z 
-      var u = this.fade(x); 
-      var v = this.fade(y); 
-      var w = this.fade(z); 
-       // Interpolate along x the contributions from each of the corners 
-      var nx00 = this.mix(n000, n100, u); 
-      var nx01 = this.mix(n001, n101, u); 
-      var nx10 = this.mix(n010, n110, u); 
-      var nx11 = this.mix(n011, n111, u); 
-      // Interpolate the four results along y 
-      var nxy0 = this.mix(nx00, nx10, v); 
-      var nxy1 = this.mix(nx01, nx11, v); 
-      // Interpolate the two last results along z 
-      var nxyz = this.mix(nxy0, nxy1, w); 
-
-      return nxyz; 
-    };
-
+             if (x < 0) {
+               x = -x;
+             }
+             if (y < 0) {
+               y = -y;
+             }
+             if (z < 0) {
+               z = -z;
+             }
+ 
+             var xi = Math.floor(x),
+               yi = Math.floor(y),
+               zi = Math.floor(z);
+             var xf = x - xi;
+             var yf = y - yi;
+             var zf = z - zi;
+             var rxf, ryf;
+ 
+             var r = 0;
+             var ampl = 0.5;
+ 
+             var n1, n2, n3;
+ 
+             for (var o = 0; o < perlin_octaves; o++) {
+               var of = xi + (yi << PERLIN_YWRAPB) + (zi << PERLIN_ZWRAPB);
+ 
+               rxf = scaled_cosine(xf);
+               ryf = scaled_cosine(yf);
+ 
+               n1 = perlin[of & PERLIN_SIZE];
+               n1 += rxf * (perlin[(of + 1) & PERLIN_SIZE] - n1);
+               n2 = perlin[(of + PERLIN_YWRAP) & PERLIN_SIZE];
+               n2 += rxf * (perlin[(of + PERLIN_YWRAP + 1) & PERLIN_SIZE] - n2);
+               n1 += ryf * (n2 - n1);
+ 
+               of += PERLIN_ZWRAP;
+               n2 = perlin[of & PERLIN_SIZE];
+               n2 += rxf * (perlin[(of + 1) & PERLIN_SIZE] - n2);
+               n3 = perlin[(of + PERLIN_YWRAP) & PERLIN_SIZE];
+               n3 += rxf * (perlin[(of + PERLIN_YWRAP + 1) & PERLIN_SIZE] - n3);
+               n2 += ryf * (n3 - n2);
+ 
+               n1 += scaled_cosine(zf) * (n2 - n1);
+ 
+               r += n1 * ampl;
+               ampl *= perlin_amp_falloff;
+               xi <<= 1;
+               xf *= 2;
+               yi <<= 1;
+               yf *= 2;
+               zi <<= 1;
+               zf *= 2;
+ 
+               if (xf >= 1.0) {
+                 xi++;
+                 xf--;
+               }
+               if (yf >= 1.0) {
+                 yi++;
+                 yf--;
+               }
+               if (zf >= 1.0) {
+                 zi++;
+                 zf--;
+               }
+             }
+             return r;
+           }
